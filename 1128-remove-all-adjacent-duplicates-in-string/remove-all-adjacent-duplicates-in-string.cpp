31b61c5025f5c443a0558e0fd69911501ff51eec
Gurvansh Gupta <gurvxnsh.gupta@gmail.com>
2025-02-08 13:24:01 +0530

    Time: 44 ms (19.27%) | Memory: 12.4 MB (97.51%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -3,7 +3,7 @@ public:
     string removeDuplicates(string s) {
     int i=0;
     while(i<s.length()){
-        if(i<s.length()-1 && i>=0){
+        if(i<s.length()-1){
             if(s[i]==s[i+1]){
                 s.erase(i,2);
                 i-=2;
@@ -14,7 +14,6 @@ public:
             i=0;
         }
     }
-    cout<<i;
     return s;
     }
 };
